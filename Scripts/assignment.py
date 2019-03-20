f682e4cc386feee0fc499f8acd8bb6808f2a5bba
West Jens <Jens.West@hsl.fi>
2019-03-20 17:29:22 +0200

    Transit assignment added.

--- Scripts/assignment.py
+++ Scripts/assignment.py
@@ -54,6 +54,7 @@ class AssignmentModel:
         self.process(function_file)
         self.calc_road_cost(scen_id)
         self.assign_cars(scen_id, param.stopping_criteria_coarse)
+        self.assign_transit(scen_id)
 
     # def create_matrix(self, id, name="", description=""):
         # emme_mtx = self.emme_modeller.emmebank.matrix(id)
@@ -162,4 +163,256 @@ class AssignmentModel:
         pass
         
     def assign_transit(self, scen_id):
-        pass
+        """Perform transit assignment for one scenario."""
+        emmebank = self.emme_modeller.emmebank
+        scenario = emmebank.scenario(scen_id)
+        network = scenario.get_network()
+        # Calculation of cumulative line segment travel time and speed
+        self.logger.info("Calculates cumulative travel times for scenario "  + str(scen_id))
+        for line in network.transit_lines():
+            cumulative_length = 0
+            cumulative_time = 0
+            cumulative_speed = 0
+            headway_sd = 0
+            for segment in line.segments():
+                cumulative_length += segment.link.length
+                # Travel time for buses in mixed traffic
+                if segment.transit_time_func == 1:
+                    cumulative_time += ( segment.data2 * segment.link.length
+                                       # + segment.link["@timau"]
+                                       + segment.link.auto_time
+                                       + segment.dwell_time)
+                # Travel time for buses on bus lanes
+                if segment.transit_time_func == 2:
+                    cumulative_time += ( segment.data2 * segment.link.length
+                                       + segment.dwell_time)
+                # Travel time for trams AHT
+                if segment.transit_time_func == 3:
+                    speedstr = str(int(segment.link.data1))
+                    # Digits 5-6 from end (1-2 from beg.) represent AHT speed.
+                    # If AHT speed is less than 10, data1 will have only 5 digits.
+                    speed = int(speedstr[:-4])
+                    cumulative_time += ((segment.link.length / speed) * 60
+                                       + segment.dwell_time)
+                # Travel time for trams PT
+                if segment.transit_time_func == 4:
+                    speedstr = str(int(segment.link.data1))
+                    # Digits 3-4 from end represent PT speed.
+                    speed = int(speedstr[-4:-2])
+                    cumulative_time += ((segment.link.length / speed) * 60
+                                       + segment.dwell_time)
+                # Travel time for trams IHT
+                if segment.transit_time_func == 5:
+                    speedstr = str(int(segment.link.data1))
+                    # Digits 1-2 from end represent IHT speed.
+                    speed = int(speedstr[-2:])
+                    cumulative_time += ((segment.link.length / speed) * 60
+                                       + segment.dwell_time)
+                if (cumulative_time > 0):
+                    cumulative_speed = cumulative_length / cumulative_time * 60
+                # Headway standard deviation for buses
+                if (line.mode.id == 'b' or line.mode.id == 'd'):
+                    headway_sd = ( 2.164 
+                                 + 0.078 * cumulative_time 
+                                 - 0.028 * cumulative_speed)
+                # Headway standard deviation for trunk buses
+                if (line.mode.id == 'g'):
+                    headway_sd = ( 2.127 
+                                 + 0.034 * cumulative_time 
+                                 - 0.021 * cumulative_speed)
+                # Headway standard deviation for trams
+                if (line.mode.id == 't'):
+                    headway_sd = ( 1.442 
+                                 + 0.060 * cumulative_time 
+                                 - 0.039 * cumulative_speed)
+                # Headway standard deviation for light rail
+                if (line.mode.id == 'p'):
+                    headway_sd = ( 1.442 
+                                 + 0.034 * cumulative_time 
+                                 - 0.039 * cumulative_speed)
+                # Estimated waiting time addition caused by headway deviation
+                segment["@wait_time_dev"] = headway_sd**2 / (2*line.headway)
+        scenario.publish_network(network)
+        # Definition of line specific boarding penalties
+        netw_specs = []
+        # Bus
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=b",
+            },
+            "expression": "3",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # Trunk bus
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=g",
+            },
+            "expression": "3",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # Long-distance and express buses
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=de",
+            },
+            "expression": "5",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # Train
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=rj",
+            },
+            "expression": "2",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # Metro and ferry
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=mw",
+            },
+            "expression": "0",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # Tram and light rail
+        netw_specs.append({
+            "type": "NETWORK_CALCULATION",
+            "selections": {
+                "transit_line": "mode=tp",
+            },
+            "expression": "0",
+            "result": "ut3",
+            "aggregation": None,
+        })
+        # netw_specs.append({
+            # "type": "NETWORK_CALCULATION",
+            # "selections": {
+                # "link": "all",
+                # "transit_line": "mode=bgde",
+            # },
+            # "expression": "0.1*index2",
+            # "result": "us3",
+            # "aggregation": None,
+        # })
+        netcalc = self.emme_modeller.tool(
+            "inro.emme.network_calculation.network_calculator")
+        netcalc(netw_specs, scenario)
+        # Definition of transition rules: all modes are allowed
+        transitions = []
+        for mode in param.transit_modes:
+            transitions.append({
+                "mode": mode,
+                "next_journey_level": 1
+            })
+        func = {
+            "type": "BPR",
+            "weight": 1.23,
+            "exponent": 3,
+            "assignment_period": 1,
+            "orig_func": False,
+            "congestion_attribute": "us3",
+        }
+        # func = {
+            # "type": "CUSTOM",
+            # "assignment_period": 1,
+            # "orig_func": False,
+            # "congestion_attribute": "us3",
+            # "python_function": """def calc_segment_cost(transit_volume, capacity, segment):
+                                    # return 0.53 * ((transit_volume / capacity) ** 2)"""
+        # }
+        stop = {
+            "max_iterations": 10,
+            "normalized_gap": 0.01,
+            "relative_gap": 0.001
+        }
+        self.logger.info("Transit assignment started")
+        transit_assignment = self.emme_modeller.tool(
+            "inro.emme.transit_assignment.extended_transit_assignment")
+        congested_assignment = self.emme_modeller.tool(
+            "inro.emme.transit_assignment.congested_transit_assignment")
+        transit_assignment(param.trass_spec, scenario)
+        # congested_assignment(transit_assignment_spec=trass_spec, 
+                             # congestion_function=func,
+                             # stopping_criteria=stop, 
+                             # log_worksheets=False, 
+                             # scenario=scenario)
+        
+        # tottim_id = "mf" + result_mat_id + "0"
+        # inveht_id = "mf" + result_mat_id + "1"
+        # auxtim_id = "mf" + result_mat_id + "2"
+        # twtime_id = "mf" + result_mat_id + "3"
+        # fwtime_id = "mf" + result_mat_id + "4"
+        # boatim_id = "mf" + result_mat_id + "5"
+        # noboa_id = "mf" + result_mat_id + "6"
+        # invlen_id = "mf" + result_mat_id + "7"
+        # create_matrix = emme_modeller.tool(
+            # "inro.emme.data.matrix.create_matrix")
+        # create_matrix(matrix_id=tottim_id,
+                      # matrix_name="tottim",
+                      # matrix_description="total time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=inveht_id,
+                      # matrix_name="inveht",
+                      # matrix_description="in veh time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=auxtim_id,
+                      # matrix_name="auxtim",
+                      # matrix_description="aux time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=twtime_id,
+                      # matrix_name="twtime",
+                      # matrix_description="tot wait time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=fwtime_id,
+                      # matrix_name="fwtime",
+                      # matrix_description="first wait time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=boatim_id,
+                      # matrix_name="boatim",
+                      # matrix_description="board time s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=noboa_id,
+                      # matrix_name="noboa",
+                      # matrix_description="no of board s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # create_matrix(matrix_id=invlen_id,
+                      # matrix_name="invlen",
+                      # matrix_description="in veh lenght s="+str(scen_id),
+                      # default_value=0,
+                      # overwrite=True)
+        # result_spec = {
+            # "type": "EXTENDED_TRANSIT_MATRIX_RESULTS",
+            # "total_impedance": tottim_id,
+            # "actual_first_waiting_times": fwtime_id,
+            # "actual_total_waiting_times": twtime_id,
+            # "by_mode_subset": {
+                # "modes": transit_modes,
+                # "distance": invlen_id,
+                # "avg_boardings": noboa_id,
+                # "actual_total_boarding_times": boatim_id,
+                # "actual_in_vehicle_times": inveht_id,
+                # "actual_aux_transit_times": auxtim_id,
+            # },
+        # }
+        matrix_results = self.emme_modeller.tool(
+            "inro.emme.transit_assignment.extended.matrix_results")
+        matrix_results(param.result_spec, scenario)
+        self.logger.info("Transit assignment performed for scenario " + str(scen_id))
--- Scripts/parameters.py
+++ Scripts/parameters.py
@@ -22,15 +22,34 @@ emme_scenario = {
     "pt": 22,
     "iht": 23,
 }
+transit_modes = [
+    'b',
+    'd',
+    'e',
+    'g',
+    'j',
+    'm',
+    'p',
+    'r',
+    't',
+    'w',
+]
+aux_modes = [
+    'a',
+    's',
+]
+transit_assignment_modes = transit_modes + aux_modes
 emme_mtx = {
     "demand": {
         "car": "mf1",
         "trailer_truck": "mf71",
         "truck": "mf72",
         "van": "mf73",
+        "transit": "mf4",
     },
     "time": {
         "car": "mf380",
+        "transit": "mf20"
     },
     "dist": {
         "car": "mf381",
@@ -74,6 +93,11 @@ demand_share = {
         "iht": 0.1,
     },
     "van": {
+        "aht": 0.1,
+        "pt": 0.05,
+        "iht": 0.1,
+    },
+    "transit": {
         "aht": 0.1,
         "pt": 0.05,
         "iht": 0.1,
@@ -187,4 +211,132 @@ vans = {
         }
     },
     "path_analyses": []
+}
+transitions = []
+for mode in transit_modes:
+    transitions.append({
+        "mode": mode,
+        "next_journey_level": 1
+    })
+# Specification of the transit assignment
+trass_spec = {
+    "type": "EXTENDED_TRANSIT_ASSIGNMENT",
+    "modes": transit_assignment_modes,
+    "demand": emme_mtx["demand"]["transit"],
+    "waiting_time": {
+        "headway_fraction": 0.5,
+        "effective_headways": "hdw",
+        "spread_factor": 1,
+        "perception_factor": 1.5
+    },
+    # Boarding time is defined for each journey level separately,
+    # so here we just set the default to zero.
+    "boarding_time": {
+        "global": {
+            "penalty": 0,
+            "perception_factor": 1,
+        },
+        "at_nodes": None,
+        "on_lines": None,
+        "on_segments": None,
+    },
+    "boarding_cost": {
+        "global": {
+            "penalty": 0,
+            "perception_factor": 1,
+        },
+        "at_nodes": None,
+        "on_lines": None,
+        "on_segments": None,
+    },
+    "in_vehicle_time": {
+        "perception_factor": 1
+    },
+    "in_vehicle_cost": None,
+    "aux_transit_time": {
+        "perception_factor": 1.75
+    },
+    "aux_transit_cost": None,
+    "flow_distribution_at_origins": {
+        "choices_at_origins": "OPTIMAL_STRATEGY",
+        "fixed_proportions_on_connectors": None
+    },
+    "flow_distribution_at_regular_nodes_with_aux_transit_choices": {
+        "choices_at_regular_nodes": "OPTIMAL_STRATEGY"
+    },
+    "flow_distribution_between_lines": {
+        "consider_total_impedance": False
+    },
+    "connector_to_connector_path_prohibition": None,
+    "od_results": {
+        "total_impedance": None
+    },
+    # The two journey levels are identical, except that at the second
+    # level an extra boarding penalty is implemented,
+    # hence a transfer penalty. Walk only trips are not allowed.
+    "journey_levels": [
+        {
+            "description": "Not boarded yet",
+            "destinations_reachable": False,
+            "transition_rules": transitions,
+            "boarding_time": {
+                "global": None,
+                "at_nodes": None,
+                "on_lines": {
+                    "penalty": "ut3",
+                    "perception_factor": 1
+                },
+                "on_segments": {
+                    "penalty": "@wait_time_dev",
+                    "perception_factor": 3.5
+                },
+            },
+            "boarding_cost": None,
+            "waiting_time": None
+        },
+        {
+            "description": "Boarded at least once",
+            "destinations_reachable": True,
+            "transition_rules": transitions,
+            "boarding_time": {
+                "global": None,
+                "at_nodes": None,
+                "on_lines": {
+                    "penalty": "ut3",
+                    "perception_factor": 1
+                },
+                "on_segments": {
+                    "penalty": "@wait_time_dev",
+                    "perception_factor": 3.5
+                }
+            },
+            "boarding_cost": {
+                "global": {
+                    "penalty": 5,
+                    "perception_factor": 1,
+                },
+                "at_nodes": None,
+                "on_lines": None,
+                "on_segments": None,                    
+            },
+            "waiting_time": None
+        }
+    ],
+    "performance_settings": {
+        "number_of_processors": "max"
+    },
+}
+result_spec = {
+    "type": "EXTENDED_TRANSIT_MATRIX_RESULTS",
+    "total_impedance": emme_mtx["time"]["transit"],
+    "actual_first_waiting_times": None,
+    "actual_total_waiting_times": None,
+    "by_mode_subset": {
+        "modes": transit_modes,
+        "distance": None,
+        "avg_boardings": None,
+        "actual_total_boarding_times": None,
+        "actual_in_vehicle_times": None,
+        "actual_aux_transit_times": None,
+    },
 }
