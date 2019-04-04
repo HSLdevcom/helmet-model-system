d3ed454fa3386783e1aa3b55d58aad63da3ac8f5
EXT- Markus Paasovaara <EXT-Markus.Paasovaara@hsl.fi>
2019-04-04 10:21:40 +0300

    Added some logging

--- Scripts/app.py
+++ Scripts/app.py
@@ -7,9 +7,13 @@ import departure_time as dt
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
+logger = logging.getLogger()
+
 script_dir = os.path.dirname(os.path.realpath('__file__'))
 project_dir = os.path.join(script_dir, "..")
 matrix_dir = os.path.join(project_dir, "Matrices")
+logger.info("Reading Matrices from " + str(matrix_dir))
+
 ass_model = TestAssignmentModel(matrix_dir)
 dtm = dt.DepartureTimeModel(ass_model)
 # nr_zones = len(ass_model.get_zone_numbers())
@@ -32,7 +36,12 @@ demand = {
         "van": car_matrix,
     },
 }
+
+logger.info("Adding demand and assigning")
+
 for purpose in demand:
     for mode in demand[purpose]:
         dtm.add_demand(purpose, mode, demand[purpose][mode])
 dtm.assign()
+
+logger.info("Done")
