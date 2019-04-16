f6be16072179f9b9cb2a915937a141f3d3ed7360
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-04-16 16:22:46 +0300

    Try out different version of numpy and python2

--- Scripts/import-python.bat
+++ Scripts/import-python.bat
@@ -1,3 +1,5 @@
 @REM This script imports all required Python dependencies to system PATH for this session
-set EMME_PYTHON_PATH=%ProgramFiles%/INRO/Emme/Emme 4/Emme-4.3.3/Python27/
-set PATH=%PATH%;%APPDATA%/Python/Scripts/;%EMME_PYTHON_PATH%
+@REM set EMME_PYTHON_PATH=%ProgramFiles%/INRO/Emme/Emme 4/Emme-4.3.3/Python27/
+@REM set PATH=%PATH%;%APPDATA%/Python/Scripts/;%EMME_PYTHON_PATH%
+set MY_PYTHON_PATH=C:/Python27/
+set PATH=%MY_PYTHON_PATH%;%MY_PYTHON_PATH%/Scripts/;%APPDATA%/Python/Scripts/;%PATH%
