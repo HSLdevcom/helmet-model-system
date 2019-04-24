1f5be16673cf28d2499e36c6e3d2ee379fee1029
West Jens <Jens.West@hsl.fi>
2019-04-24 15:38:23 +0300

    Added destination choice averaging for fast growing zones.

--- Scripts/demand/freight.py
+++ Scripts/demand/freight.py
@@ -15,16 +15,32 @@ class FreightModel:
         production_forecast = self.generate_trips(zone_data_forecast, mode)
         # Remove zero values
         base_mtx = self.base_demand.get_data(mode).clip(0.000001, None) 
-        external_growth = 1.1
-        production_calib = base_mtx.sum(1)
-        production = self.calibrate(production_calib[:len(production_base)], 
+        production = self.calibrate(base_mtx.sum(1), #[:len(production_base)], 
                                     production_base, 
                                     production_forecast)
-        # External demand is generated for the last centroids
-        external_base = production_calib[len(production):]
-        external_production = external_growth * external_base
-        production = numpy.append(production, external_production)
-        # Add harbour traffic!
+        # # External demand is generated for the last centroids
+        # external_base = production_calib[len(production):]
+        # external_growth = 1.1
+        # external_production = external_growth * external_base
+        # production = numpy.append(production, external_production)
+        zone_numbers = self.base_demand.get_zone_numbers()
+        pd_mtx = pandas.DataFrame(base_mtx, zone_numbers, zone_numbers)
+        ave1000 = pandas.DataFrame(1, zone_numbers, zone_numbers)
+        for i in xrange(0, 30):
+            l = i * 1000
+            u = l + 999
+            sum1000 = pd_mtx.loc[l:u].sum()
+            scaling = 1 / max(sum1000.sum(), 1)
+            ave1000.loc[l:u] = ave1000.loc[l:u] * sum1000 * scaling
+        # If forecast>5*base, destination choice is replaced by area average
+        cond = production_forecast < 5*production_base
+        base_mtx = numpy.where(cond, base_mtx, ave1000*production)
+        # The same check is done also for other axis
+        cond1 = cond.values[:, numpy.newaxis]
+        prod1 = production[:, numpy.newaxis]
+        ave1000T = ave1000.values.T
+        base_mtx = numpy.where(cond1, base_mtx, ave1000T*prod1)
+        # TODO Add harbour traffic!
         demand = self.fratar(production, base_mtx)
         return demand
 
@@ -44,7 +60,7 @@ class FreightModel:
         prod = Production target as array
         trips = Seed trip matrix
         max_iter (optional) = maximum iterations, default is 10
-        Returns fratared trip matrix
+        Return fratared trip matrix
         """
         #Run 2D balancing
         for _ in xrange(0, max_iter):
@@ -55,9 +71,9 @@ class FreightModel:
         return trips
 
     def calibrate(self, b, n, s):
-        """Calibrate a vector
-        b = true value for base
-        n = forecast for base
-        s = forecast to calibrate
+        """Calibrate a vector\n
+        b = true value for base\n
+        n = forecast for base\n
+        s = forecast to calibrate\n
         """
-        return numpy.where(s < 5*b, s * b/n, s + 5*(b - n))
+        return numpy.where(s < 5*n, s * b/n, s + 5*(b - n))
