0217a02d07d9e7a3741fdefbf96dd5cc4252eaa3
West Jens <Jens.West@hsl.fi>
2019-07-08 10:43:21 +0300

    Added external-external trips and comments

--- Scripts/demand/external.py
+++ Scripts/demand/external.py
@@ -9,16 +9,36 @@ class ExternalModel:
         self.growth = zone_data.externalgrowth
 
     def calc_external(self, mode, internal_trips):
+        """Calculate external traffic.
+
+        Parameters
+        ----------
+        mode : str
+            Travel mode (car/transit/truck/trailer_truck)
+        internal_trips : numpy array
+            Sums of all (intra-area) trips to and frome zones
+        
+        Return
+        ------
+        numpy 2-d matrix
+            Matrix of whole day trips from external to internal zones
+        """
         base_mtx = self.base_demand.get_external(mode)
         mtx = pandas.DataFrame(0, self.zone_numbers, self.growth[mode].index)
         internal_trips = pandas.Series(internal_trips, self.zone_numbers)
         municipalities = parameters.municipality
-        for municipality, base_vector in base_mtx.iterrows():
-            l = municipalities[municipality][0]
-            u = municipalities[municipality][1]
-            zone_weights = internal_trips.loc[l:u]
-            zone_weights = zone_weights / zone_weights.sum()
-            mtx.loc[l:u] = ( self.growth[mode].values
-                           * zone_weights[:, numpy.newaxis]
-                           * base_vector.values)
+        # Base matrix is aggregated to municipality level
+        for target, base_vector in base_mtx.iterrows():
+            if target in municipalities:
+                l = municipalities[target][0]
+                u = municipalities[target][1]
+                zone_trips = internal_trips.loc[l:u]
+                zone_weights = zone_trips / zone_trips.sum()
+                # Disaggregate base matrix to zone level and 
+                # multiply by growth factors
+                mtx.loc[l:u] = ( self.growth[mode].values
+                            * zone_weights[:, numpy.newaxis]
+                            * base_vector.values)
+            else: # External-external trips
+                mtx.loc[target] = self.growth[mode] * base_vector
         return mtx.values.T
