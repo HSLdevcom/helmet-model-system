e2dcd1ce09a40124393f17240fb737f0e0018d63
West Jens <Jens.West@hsl.fi>
2019-07-11 10:58:21 +0300

    First draft of complete Helmet app

--- Scripts/helmet_app.py
+++ Scripts/helmet_app.py
@@ -4,38 +4,70 @@ import os
 from assignment.abstract_assignment import AssignmentModel
 from assignment.emme_assignment import EmmeAssignmentModel
 from assignment.mock_assignment import MockAssignmentModel
-from data_handling import MatrixData
+import assignment.departure_time as dt
+from data_handling import ZoneData, MatrixData
+from demand.freight import FreightModel
+from demand.trips import DemandModel
+from demand.external import ExternalModel
+from transform.impedance_transformer import ImpedanceTransformer
+from datatypes.purpose import create_purposes
+from emme.emme_context import EmmeContext
+import parameters
+import numpy
 
 class HelmetApplication():
 
     def __init__(self, config):
-        self.__config = config
+        self._config = config
         self.logger = Log.get_instance()
         self.logger.info("Initializing the application..")
         # TODO clean up the model initialization and initialize other relevant classes
+        self.zdata_base = ZoneData("2016")
+        self.zdata_forecast = ZoneData(self._config.get_value(Config.DATA_PATH))
+        self.basematrices = MatrixData("base")
+        self.dm = DemandModel(self.zdata_forecast)
+        self.fm = FreightModel(self.zdata_base, self.zdata_forecast, self.basematrices)
+        self.em = ExternalModel(self.basematrices, self.zdata_forecast)
+        self.emme_context = EmmeContext(self._config.get_value(Config.EMME_PROJECT_PATH))
         # We could also perhaps wrap these under some class..?
         if config.get_value(Config.USE_EMME):
             self.logger.info("Configuration set to use EMME, initializing")
             self.initialize_EMME()
-            self.assignment_model = EmmeAssignmentModel(self.emme_context)
+            self.ass_model = EmmeAssignmentModel(self.emme_context)
         else:
             self.logger.info("Configuration NOT using EMME")
             costs = MatrixData("2016")
-            self.assignment_model = MockAssignmentModel(costs)
-
+            self.ass_model = MockAssignmentModel(costs)
+        self.dtm = dt.DepartureTimeModel(self.ass_model)
+        self.imptrans = ImpedanceTransformer(self.ass_model)
+        self.ass_classes = dict.fromkeys(parameters.emme_mtx["demand"].keys())
+        self.tour_purposes = create_purposes()
     
     def run(self):
         self.logger.info("Launching application..")
-        if not self.validate_input():
+        if not self._validate_input():
             self.logger.error("Failed to validate input, aborting simulation")
             return
+        self.trucks = self.fm.calc_freight_traffic("truck")
+        self.trailer_trucks = self.fm.calc_freight_traffic("trailer_truck")
+        impedance = {}
+        for tp in parameters.emme_scenario:
+            base_demand = {}
+            self.basematrices.open_file("demand", tp)
+            for ass_class in self.ass_classes:
+                base_demand[ass_class] = self.basematrices.get_data(ass_class)
+            self.basematrices.close()
+            self.ass_model.assign(tp, base_demand)
+            if tp == "aht":
+                self.ass_model.calc_transit_cost()
+            impedance[tp] = self.ass_model.get_impedance()
 
-        iterations = self.__config.get_value(Config.ITERATION_COUNT)
-        self.logger.info("Start simulation loop with {} iterations".format(self.__config.get_value(Config.ITERATION_COUNT)))
+        iterations = self._config.get_value(Config.ITERATION_COUNT)
+        self.logger.info("Start simulation loop with {} iterations".format(self._config.get_value(Config.ITERATION_COUNT)))
         for round in range(1, iterations+1):
             try:
                 self.logger.info("Starting round {}".format(round))
-                self.simulate()
+                impedance = self.simulate(impedance)
             except Exception as error:
                 is_fatal = self.handle_error("Exception at simulation round {}".format(round), error)
                 if is_fatal:
@@ -49,18 +81,57 @@ class HelmetApplication():
         fatal = True
         return fatal
 
-    def simulate(self):
-        #TODO Put the simulation logic for this round here
-        pass
+    def simulate(self, impedance):
+        self.dtm.add_demand("freight", "truck", self.trucks)
+        self.dtm.add_demand("freight", "trailer_truck", self.trailer_trucks)
+        for purpose in self.tour_purposes:
+            purpose_impedance = self.imptrans.transform(purpose, impedance)
+            demand = self.dm.calc_demand(purpose, purpose_impedance)
+            self._validate_demand(demand)
+            if purpose.area == "peripheral":
+                pos = self.ass_model.get_mapping()[16001]
+                mtx_position = (pos, 0)
+            else:
+                mtx_position = (0, 0)
+            if purpose.dest != "source":
+                for mode in demand:
+                    self.dtm.add_demand(purpose.name, mode, demand[mode], mtx_position)
+        pos = self.ass_model.get_mapping()[31001]
+        for mode in parameters.external_modes:
+            if mode == "truck":
+                int_demand = self.trucks.sum(0) + self.trucks.sum(1)
+            elif mode == "trailer_truck":
+                int_demand = self.trailer_trucks.sum(0) + self.trailer_trucks.sum(1)
+            else:
+                nr_zones = len(self.zdata_base.zone_numbers)
+                int_demand = numpy.zeros(nr_zones)
+                for purpose in self.tour_purposes:
+                    if purpose.dest != "source":
+                        l, u = self.zdata_base.get_bounds(purpose)
+                        int_demand[l:u] += purpose.generated_tours[mode]
+                        int_demand += purpose.attracted_tours[mode]
+            ext_demand = self.em.calc_external(mode, int_demand)
+            self.dtm.add_demand("external", mode, ext_demand, (pos, 0))
+        impedance = {}
+        for tp in parameters.emme_scenario:
+            self.dtm.add_vans(tp)
+            self.ass_model.assign(tp, self.dtm.demand[tp])
+            impedance[tp] = self.ass_model.get_impedance()
+        self.dtm.init_demand()
+        return impedance
 
-    def validate_input(self):
+    def _validate_input(self):
+        # TODO read the scenario from parameters / config and read input data & validate it
+        return True
+
+    def _validate_demand(self, demand):
         # TODO read the scenario from parameters / config and read input data & validate it
         return True
 
     def initialize_EMME(self):
         #TODO figure out if we only need to do this once in the beginning or between simulations?
         from emme.emme_context import EmmeContext
-        empfile = self.__config.get_value(Config.EMME_PROJECT_PATH)
+        empfile = self._config.get_value(Config.EMME_PROJECT_PATH)
         self.emme_context = EmmeContext(empfile)
 
 
