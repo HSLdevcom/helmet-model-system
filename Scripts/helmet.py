2ec301e4c417338ba6109248e06866f952b2127b
West Jens <Jens.West@hsl.fi>
2019-09-27 14:59:30 +0300

    Updated logging

--- Scripts/helmet.py
+++ Scripts/helmet.py
@@ -6,7 +6,7 @@ from utils.config import Config
 
 config = Config.read_from_file(path="dev-config.json")
 log = Log.get_instance().initialize(config)
-log.info('Running helmet_app.py via Helmet UI..')
+log.info('Running helmet_app.py via Helmet command line app..')
 log.debug('sys.version_info=' + str(sys.version_info[0]))
 log.debug('sys.path=' + str(sys.path))
 app = HelmetApplication(config)
