3b911a3bcd2a95f37df7f3f1631593e69b8f445f
West Jens <Jens.West@hsl.fi>
2019-09-27 14:43:43 +0300

    Added command line version of helmet app

---
+++ Scripts/helmet.py
@@ -0,0 +1,13 @@
+import sys
+import json
+from utils.log import Log
+from helmet_app import HelmetApplication
+from utils.config import Config
+
+config = Config.read_from_file(path="dev-config.json")
+log = Log.get_instance().initialize(config)
+log.info('Running helmet_app.py via Helmet UI..')
+log.debug('sys.version_info=' + str(sys.version_info[0]))
+log.debug('sys.path=' + str(sys.path))
+app = HelmetApplication(config)
+app.run()
