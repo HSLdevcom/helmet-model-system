c30c284403694f6237f2eda40579b43f2e96304e
West Jens <Jens.West@hsl.fi>
2019-10-09 10:50:40 +0300

    New car use parameters

--- Scripts/parameters.py
+++ Scripts/parameters.py
@@ -1871,21 +1871,20 @@ garbage_generation = {
     "workplaces": 0.000025,
 }
 car_usage = {
-    "constant": 1.184,
-    "generation": {
-        "downtown": -0.906,
-    },
+    "constant": -11.2296,
+    "generation": {},
     "log": {
-        "population_density": -0.275,
+        "cars_per_1000": 2.0804,
+        "population_density": -0.1471,
     },
     "individual_dummy": {
-        "share_age_18-29_f": 1.15,
-        "share_age_18-29_m": 1.44,
-        "share_age_30-49_f": 2.33,
-        "share_age_30-49_m": 2.60,
-        "share_age_50-64_f": 2.09,
-        "share_age_50-64_m": 3.26,
-        "share_age_65-99_m": 3.10,
+        "share_age_7-17": -9999,
+        "share_age_18-29_m": 0.2969,
+        "share_age_30-49_f": 1.1019,
+        "share_age_30-49_m": 1.4157,
+        "share_age_50-64_f": 0.8173,
+        "share_age_50-64_m": 1.9978,
+        "share_age_65-99_m": 1.8737,
     },
 }
 vector_calibration_threshold = 5
