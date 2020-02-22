6b1faecba487a369a3285a616f8ca6dce89a4fd6
Mew-www <mew.proxy@hotmail.com>
2020-02-22 21:41:54 +0200

    Add monday notes

--- Scripts/emme/emme_context.py
+++ Scripts/emme/emme_context.py
@@ -6,6 +6,8 @@ import inro.modeller as _m
 
 
 # Creates and initializes EMME-resources
+# TODO MON: Consider a rename. Usually 3rd party API is called "x-bindings" or "bindings for X". EmmeProjectBindings? Or just EmmeProject?
+# TODO MON: or e.g. /emme_bindings/emme_project.py as indicator "this class wraps a project path, giving API using that"
 class EmmeContext:
     def __init__(self, filepath):
         self.logger = Log.get_instance()
