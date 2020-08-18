694502f9c8cda17bb3f1c994000c2dd5512d0eb0
West Jens <Jens.West@hsl.fi>
2020-08-18 14:43:32 +0300

    Change test to match updated input files

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -37,7 +37,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.56742957061235355)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.5576934871486231)
         
         print("Model system test done")
     
