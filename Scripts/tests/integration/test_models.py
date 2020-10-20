8dd6372e3e5cfecb69b37537ca87a21f74925264
atte-wsp <atte.supponen@wsp.com>
2020-10-20 12:32:07 +0300

    Update test_models.py
    * Mode share changed due to the new parameters

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -37,7 +37,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.73084857032579786)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.73070642950251097)
         
         print("Model system test done")
     
