60b0963822bf1bbdc297ab589ce616a20b894f69
West Jens <Jens.West@hsl.fi>
2020-11-03 15:56:26 +0200

    Added matrix file validation

--- Scripts/helmet_validate_inputfiles.py
+++ Scripts/helmet_validate_inputfiles.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import os
+import numpy
 
 from utils.config import Config
 from utils.log import Log
@@ -7,6 +8,7 @@ from assignment.emme_assignment import EmmeAssignmentModel
 from datahandling.matrixdata import MatrixData
 from datahandling.zonedata import ZoneData
 from assignment.emme_bindings.emme_project import EmmeProject
+import parameters.assignment as param
 
 
 def main(args, logger):
@@ -47,8 +49,34 @@ def main(args, logger):
     # Check base zonedata
     assignment_model = EmmeAssignmentModel(EmmeProject(emme_paths[0]), first_scenario_id=first_scenario_ids[0])
     base_zonedata = ZoneData(base_zonedata_path, assignment_model.zone_numbers)
-    # Check base matrices TODO by @zptro
-    # matrixdata = MatrixData(base_matrices_path)
+    # Check base matrices
+    matrixdata = MatrixData(base_matrices_path)
+    for tp in assignment_model.emme_scenarios:
+        # TODO Move checks to MatrixData
+        with matrixdata.open("demand", tp) as mtx:
+            path = os.path.join(base_matrices_path, "demand_"+tp+".omx")
+            # TODO Get these as numpy arrays from source
+            ass_numbers = numpy.array(assignment_model.zone_numbers)
+            mtx_numbers = numpy.array(mtx.zone_numbers)
+            if not (numpy.diff(mtx_numbers) > 0).all():
+                raise IndexError("Zone numbers not in strictly ascending order in file {}".format(path))
+            if mtx_numbers.size != ass_numbers.size or (mtx_numbers != ass_numbers).any():
+                for i in mtx_numbers:
+                    if int(i) not in ass_numbers:
+                        raise IndexError("Zone number {} from file {} not found in network".format(i, path))
+                for i in ass_numbers:
+                    if i not in mtx_numbers:
+                        raise IndexError("Zone number {} not found in file {}".format(i, path))
+                raise IndexError("Zone numbers did not match for file {}".format(path))
+            ass_classes = mtx.matrix_list
+            for ass_class in param.transport_classes:
+                if ass_class not in ass_classes:
+                    raise IndexError("File {} does not contain {} matrix.".format(
+                        path, ass_class))
+                a = mtx[ass_class]
+                if a.shape[0] != mtx_numbers.size or a.shape[1] != mtx_numbers.size:
+                    raise IndexError("Matrix {} in file {} has wrong dimensions".format(
+                        ass_class, path))
 
     # Check scenario based input data
     logger.info("Checking base zonedata & scenario-based input data...")
