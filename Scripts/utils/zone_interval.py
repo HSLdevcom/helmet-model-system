6144a4c42f3530c9182791b1bf056c47a9badb2d
West Jens <Jens.West@hsl.fi>
2021-03-19 14:35:18 +0200

    Change Helsinki CBD definition into geometrical polygon

--- Scripts/utils/zone_interval.py
+++ Scripts/utils/zone_interval.py
@@ -1,5 +1,6 @@
 import numpy
 import pandas
+from shapely.geometry import Point, Polygon
 
 import parameters.zone as param
 import utils.log as log
@@ -50,6 +51,9 @@ def is_in(interval, zone_number):
     return False
 
 
+cbd = Polygon(param.helsinki_cbd)
+
+
 def belongs_to_area(node):
     """Get name of area to which node belongs to.
 
@@ -65,7 +69,7 @@ def belongs_to_area(node):
     """
     try:
         municipality = param.kela_codes[int(node.data3)]
-        if municipality == "Helsinki" and node.label != 'A':
+        if municipality == "Helsinki" and not Point(node.x, node.y).within(cbd):
             first_zone_id = 1000
         else:
             first_zone_id = param.municipalities[municipality][0]
