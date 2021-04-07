5fd912f6e3eadf4f96af311b5fd60428a6ea9312
West Jens <Jens.West@hsl.fi>
2021-04-07 15:35:14 +0300

    Align columns better with values padded up to 12 digits Restrict number of significant digits to 7, larger numbers as exponents

--- Scripts/datahandling/resultdata.py
+++ Scripts/datahandling/resultdata.py
@@ -25,9 +25,13 @@ class ResultsData:
             self._line_buffer[filename].close()
         self._line_buffer = {}
         for filename in self._df_buffer:
-            self._df_buffer[filename].to_csv(
+            df = self._df_buffer[filename]
+            df.index = df.index.astype(str, copy=False)
+            # Pad index according to its longest item
+            df.index = df.index.str.pad(len(max(df.index, key=len)))
+            df.to_csv(
                 os.path.join(self.path, filename),
-                sep='\t', float_format="%1.5f")
+                sep='\t', na_rep=12*' ', float_format="%12.7g")
         self._df_buffer = {}
         for filename in self._xlsx_buffer:
             self._xlsx_buffer[filename].save(
@@ -46,6 +50,7 @@ class ResultsData:
         colname : str
             Desired name of this column
         """
+        colname = colname.rjust(12)
         if filename not in self._df_buffer:
             self._df_buffer[filename] = pandas.DataFrame(data, columns=[colname])
         else:
@@ -87,7 +92,7 @@ class ResultsData:
             # If no Workbook module available (= _use_txt), save data to csv
             data.to_csv(
                 os.path.join(self.path, "{}_{}.txt".format(filename, sheetname)),
-                sep='\t', float_format="%8.1f")
+                sep='\t', float_format="%12.7g")
         else:
             # Get/create new worksheet
             if filename in self._xlsx_buffer:
