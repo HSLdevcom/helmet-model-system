8ea047920af0a590c0a3767a24448be1d98ce135
West Jens <Jens.West@hsl.fi>
2021-04-12 15:11:23 +0300

    Add matrix handling and add transit cost calc test

--- Scripts/assignment/emme_bindings/mock_project.py
+++ Scripts/assignment/emme_bindings/mock_project.py
@@ -1,3 +1,4 @@
+import numpy
 from collections import namedtuple
 
 
@@ -5,8 +6,11 @@ class MockProject:
     def __init__(self):
         self.modeller = Modeller(EmmeBank())
 
-    def create_matrix(self, *args, **kwargs):
-        pass
+    def create_matrix(self, matrix_id, matrix_name, matrix_description,
+                      default_value, overwrite):
+        dimension = len(next(self.modeller.emmebank.scenarios()).zone_numbers)
+        self.modeller.emmebank._matrices[matrix_id] = Matrix(
+            matrix_id, dimension, default_value)
 
     def create_extra_attribute(self, extra_attribute_type,
                                extra_attribute_name,
@@ -16,6 +20,12 @@ class MockProject:
         extra_attr = scenario.get_network()._extra_attr[extra_attribute_type]
         extra_attr[extra_attribute_name] = extra_attribute_default_value
 
+    def matrix_results(self, *args, **kwargs):
+        pass
+
+    def transit_assignment(self, *args, **kwargs):
+        pass
+
 
 Modeller = namedtuple("Modeller", "emmebank")
 
@@ -23,6 +33,7 @@ Modeller = namedtuple("Modeller", "emmebank")
 class EmmeBank:
     def __init__(self):
         self._scenarios = {idx: Scenario(idx) for idx in range(19, 24)}
+        self._matrices = {}
 
     def scenario(self, idx):
         return self._scenarios[idx]
@@ -30,11 +41,18 @@ class EmmeBank:
     def scenarios(self):
         return iter(self._scenarios.values())
 
+    def matrix(self, idx):
+        return self._matrices[idx]
+
 
 class Scenario:
     def __init__(self, idx):
         self.id = idx
         self._network = Network()
+
+    @property
+    def zone_numbers(self):
+        return sorted(self._network._centroids)
 
     def get_network(self):
         return self._network
@@ -43,10 +61,23 @@ class Scenario:
         self._network = network
 
 
+class Matrix:
+    def __init__(self, idx, dim, default_value):
+        self.id = idx
+        self._data = numpy.full((dim, dim), default_value)
+
+    def get_numpy_data(self):
+        return self._data
+
+    def set_numpy_data(self, data):
+        self._data[:,:] = data
+
+
 class Network:
     def __init__(self):
         self._modes = {}
         self._nodes = {}
+        self._centroids = {}
         self._links = {}
         self._transit_lines = {}
         self._extra_attr = {
@@ -64,6 +95,18 @@ class Network:
 
     def node(self, idx):
         return self._nodes[idx]
+
+    def nodes(self):
+        return iter(self._nodes.values())
+
+    def centroids(self):
+        return iter(self._centroids.values())
+
+    def create_node(self, idx, is_centroid=False):
+        node = Node(self, idx, is_centroid)
+        self._nodes[idx] = node
+        if is_centroid:
+            self._centroids[idx] = node
 
     def link(self, i_node_id, j_node_id):
         return self._links[(i_node_id, j_node_id)]
@@ -95,17 +138,22 @@ class NetworkObject:
 
 
 class Node(NetworkObject):
-    def __init__(self, idx, network):
-        self.id = idx
+    def __init__(self, network, idx, is_centroid=False):
         self.network = network
+        self.is_centroid = is_centroid
+        self.id = str(idx)
+        self.number = idx
+        self.label = ""
+        self.data1 = 0
         self._extra_attr = network._extra_attr["NODE"].copy()
 
 
 class Link(NetworkObject):
-    def __init__(self, i_node, j_node, network, length):
-        self.i_node = i_node
-        self.j_node = j_node
+    def __init__(self, network, i_node, j_node, length):
         self.network = network
+        self.id = "{}-{}".format(i_node, j_node)
+        self.i_node = network.node(i_node)
+        self.j_node = network.node(j_node)
         self.length = length
         self.volume_delay_func = 0
         self._extra_attr = network._extra_attr["LINK"].copy()
@@ -117,9 +165,9 @@ class Link(NetworkObject):
 
 
 class TransitLine(NetworkObject):
-    def __init__(self, idx, network, mode, headway):
-        self.id = idx
+    def __init__(self, network, idx, mode, headway):
         self.network = network
+        self.id = idx
         self.mode = mode
         self.headway = headway
         self.data3 = 0
@@ -139,4 +187,5 @@ class TransitSegment:
         self.line = line
         self.link = link
         self._extra_attr = network._extra_attr["TRANSIT_SEGMENT"].copy()
+        line._segments.append(self)
         link._segments.append(self)
--- Scripts/tests/unit/test_assignment.py
+++ Scripts/tests/unit/test_assignment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import unittest
-from collections import namedtuple
+import numpy
 
 import assignment.emme_bindings.mock_project as mock
 from assignment.emme_assignment import EmmeAssignmentModel
@@ -14,16 +14,29 @@ class EmmeAssignmentTest(unittest.TestCase):
             network = scenario.get_network()
             for idx in ('c', 'b'):
                 network._modes[idx] = mock.Mode(idx)
+            for idx in (101, 4003, 16001, 16002):
+                network.create_node(idx, is_centroid=True)
+                network.node(idx).label = 'A'
             for idx in range(1, 5):
-                network._nodes[idx] = mock.Node(idx, network)
+                network.create_node(idx)
+                network.node(idx).label = 'A'
             for link in ((1, 2), (2, 3), (3, 4)):
-                network._links[link] = mock.Link(*link, network=network, length=3.5)
+                network._links[link] = mock.Link(network, *link, length=3.5)
             for idx in range(1, 4):
                 network._transit_lines[idx] = mock.TransitLine(
-                    idx, network, network.mode('b'), headway=5)
-            line = network.transit_line(1)
-            line._segments.append(
-                mock.TransitSegment(network, line, network.link(1, 2)))
+                    network, idx, network.mode('b'), headway=5)
+            mock.TransitSegment(
+                network, network.transit_line(1), network.link(1, 2))
 
         ass_model = EmmeAssignmentModel(context, 19)
         ass_model.prepare_network()
+        fares = {
+            "fare": {
+                'A': 59,
+            },
+            "exclusive": {},
+            "dist_fare": 3.0,
+            "start_fare": 35,
+        }
+        peripheral_cost = numpy.arange(8).reshape((2, 4))
+        ass_model.calc_transit_cost(fares, peripheral_cost)
