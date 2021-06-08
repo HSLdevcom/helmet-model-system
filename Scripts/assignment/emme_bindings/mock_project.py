01209f1ad7ee5c00368a622cca3a46a8c6a64753
Jens West <jens.west@hsl.fi>
2021-06-08 10:20:55 +0300

    Read test network from files (#318)
    * Implement file import methods and read test network from files
    
    * Add file import of extra attributes
    
    * Remove hard-coded file names
    
    * Check variable types
    
    * Add missing link types to link length aggregation

--- Scripts/assignment/emme_bindings/mock_project.py
+++ Scripts/assignment/emme_bindings/mock_project.py
@@ -1,6 +1,7 @@
 import numpy
 from collections import namedtuple
 import copy
+import os
 
 
 class MockProject:
@@ -24,6 +25,30 @@ class MockProject:
         scenario.title = scenario_title
         return scenario
 
+    def import_scenario(self, scenario_dir, scenario_id, scenario_title):
+        scenario = self.modeller.emmebank.create_scenario(scenario_id)
+        scenario.title = scenario_title
+        for file_name in os.listdir(scenario_dir):
+            if file_name.startswith("modes"):
+                self.mode_transaction(
+                    os.path.join(scenario_dir, file_name), scenario=scenario)
+        for file_name in os.listdir(scenario_dir):
+            if file_name.startswith("base_network"):
+                self.base_network_transaction(
+                    os.path.join(scenario_dir, file_name), scenario=scenario)
+        for file_name in os.listdir(scenario_dir):
+            if file_name.startswith("vehicles"):
+                self.vehicle_transaction(
+                    os.path.join(scenario_dir, file_name), scenario=scenario)
+        for file_name in os.listdir(scenario_dir):
+            if file_name.startswith("transit_lines"):
+                self.transit_line_transaction(
+                    os.path.join(scenario_dir, file_name), scenario=scenario)
+        for file_name in os.listdir(scenario_dir):
+            if file_name.startswith("extra"):
+                self.import_extra_attributes(
+                    os.path.join(scenario_dir, file_name), scenario=scenario)
+
     def create_matrix(self, matrix_id, matrix_name, matrix_description,
                       default_value=0, overwrite=False):
         try:
@@ -36,7 +61,7 @@ class MockProject:
     def create_extra_attribute(self, extra_attribute_type,
                                extra_attribute_name,
                                extra_attribute_description,
-                               extra_attribute_default_value=0,
+                               extra_attribute_default_value=0.0,
                                overwrite=False, scenario=None):
         try:
             scenario.create_extra_attribute(
@@ -53,6 +78,210 @@ class MockProject:
         eb = self.modeller.emmebank
         eb.matrix(matrix_id).set_numpy_data(
             eb.matrix(from_matrix).get_numpy_data())
+
+    def mode_transaction(self, transaction_file, revert_on_error=True,
+                         scenario=None):
+        network = scenario.get_network()
+        with open(transaction_file) as f:
+            while True:
+                if f.readline() == "t modes\n":
+                    break
+            while True:
+                rec = f.readline().split()
+                if not rec:
+                    break
+                if rec[0] == "c":
+                    pass
+                elif rec[0] == "d":
+                    # TODO Implement deletion
+                    pass
+                else:
+                    if rec[0] == "a":
+                        network.create_mode(mode_type=rec[3], idx=rec[1])
+                    elif rec[0] == "m":
+                        network.mode(idx=rec[1])
+                    else:
+                        raise SyntaxError("Unknown update code")
+
+    def base_network_transaction(self, transaction_file, revert_on_error=True,
+                                 scenario=None):
+        network = scenario.get_network()
+        with open(transaction_file) as f:
+            while True:
+                if f.readline() == "t nodes\n":
+                    break
+            while True:
+                line = f.readline()
+                if line == "t links\n":
+                    break
+                rec = line.split()
+                if rec[0] == "c":
+                    pass
+                elif rec[0] == "d":
+                    # TODO Implement deletion
+                    pass
+                else:
+                    if rec[0] == "a":
+                        node = network.create_node(rec[1], is_centroid=False)
+                    elif rec[0] == "a*":
+                        node = network.create_node(rec[1], is_centroid=True)
+                    elif rec[0] == "m":
+                        node = network.node(rec[1])
+                    else:
+                        raise SyntaxError("Unknown update code")
+                    node.x = float(rec[2])
+                    node.y = float(rec[3])
+                    node.data1 = float(rec[4])
+                    node.data2 = float(rec[5])
+                    node.data3 = float(rec[6])
+                    node.label = rec[7]
+            while True:
+                rec = f.readline().split()
+                if not rec:
+                    break
+                if rec[0] == "c":
+                    pass
+                elif rec[0] == "d":
+                    # TODO Implement deletion
+                    pass
+                else:
+                    if rec[0] == "a":
+                        link = network.create_link(
+                            i_node_id=rec[1], j_node_id=rec[2], modes=rec[4])
+                    elif rec[0] == "m":
+                        link = network.link(
+                            i_node_id=rec[1], j_node_id=rec[2])
+                    else:
+                        raise SyntaxError("Unknown update code")
+                    link.length = float(rec[3])
+                    link.type = int(rec[5])
+                    link.num_lanes = float(rec[6])
+                    link.volume_delay_func = int(rec[7])
+                    link.data1 = float(rec[8])
+                    link.data2 = float(rec[9])
+                    link.data3 = float(rec[10])
+
+    def vehicle_transaction(self, transaction_file, revert_on_error=True,
+                            scenario=None):
+        network = scenario.get_network()
+        with open(transaction_file) as f:
+            while True:
+                if f.readline() == "t vehicles\n":
+                    break
+            while True:
+                rec = f.readline().split()
+                if not rec:
+                    break
+                if rec[0] == "c":
+                    pass
+                elif rec[0] == "d":
+                    # TODO Implement deletion
+                    pass
+                else:
+                    if rec[0] == "a":
+                        vehicle = network.create_transit_vehicle(
+                            idx=int(rec[1]), mode_id=rec[3])
+                    elif rec[0] == "m":
+                        vehicle = network.transit_vehicle(idx=int(rec[1]))
+                    else:
+                        raise SyntaxError("Unknown update code")
+                    vehicle.description = rec[2][1:-1]
+
+    def transit_line_transaction(self, transaction_file, revert_on_error=True,
+                            scenario=None):
+        network = scenario.get_network()
+        with open(transaction_file) as f:
+            while True:
+                if f.readline() == "t lines\n":
+                    break
+            rec = f.readline().replace("'", " ").split()
+            while True:
+                if not rec:
+                    break
+                if rec[0] == "c":
+                    rec = f.readline().replace("'", " ").split()
+                elif rec[0] == "d":
+                    # TODO Implement deletion
+                    rec = f.readline().replace("'", " ").split()
+                else:
+                    if rec[0] == "a":
+                        line_id = rec[1]
+                        vehicle_id = int(rec[3])
+                        headway = float(rec[4])
+                        itinerary = []
+                        ttf = []
+                        data1 = []
+                        data2 = []
+                        data3 = []
+                        while True:
+                            segrec = f.readline().replace("'", " ").split()
+                            if not segrec or segrec[0] in "amd":
+                                rec = segrec
+                                break
+                            elif segrec[0] not in ("c", "path=no"):
+                                itinerary.append(segrec[0])
+                                try:
+                                    ttf.append(int(segrec[2][4:]))
+                                    data1.append(float(segrec[3][4:]))
+                                    data2.append(float(segrec[4][4:]))
+                                    data3.append(float(segrec[5][4:]))
+                                except IndexError:
+                                    pass
+                        line = network.create_transit_line(
+                            line_id, vehicle_id, itinerary)
+                        for i, segment in enumerate(line.segments()):
+                            segment.transit_time_func = ttf[i]
+                            segment.data1 = data1[i]
+                            segment.data2 = data2[i]
+                            segment.data3 = data3[i]
+                    elif rec[0] == "m":
+                        line = network.transit_line(idx=rec[1])
+                        headway = float(rec[4])
+                    else:
+                        raise SyntaxError("Unknown update code")
+                    line.vehicle = vehicle_id
+                    line.headway = headway
+
+    def import_extra_attributes(self, file_path, revert_on_error=True,
+                                scenario=None, import_definitions=False):
+        with open(file_path) as f:
+            f.readline()
+            while True:
+                rec = f.readline().split()
+                if rec[0] == "end":
+                    break
+                attr_type = rec[1]
+                self.create_extra_attribute(
+                    attr_type, rec[0], "",
+                    extra_attribute_default_value=float(rec[2]),
+                    overwrite=True, scenario=scenario)
+            header = f.readline().split()
+            network = scenario.get_network()
+            while True:
+                rec = f.readline().replace("'", " ").split()
+                if not rec:
+                    break
+                elif attr_type == "NODE":
+                    node = network.node(int(rec[0]))
+                    for i, attr in enumerate(header[1:], 1):
+                        node[attr] = float(rec[i])
+                elif attr_type == "LINK":
+                    link = network.link(int(rec[0]), int(rec[1]))
+                    for i, attr in enumerate(header[2:], 2):
+                        link[attr] = float(rec[i])
+                elif attr_type == "TRANSIT_LINE":
+                    line = network.transit_line(rec[0])
+                    for i, attr in enumerate(header[1:], 1):
+                        line[attr] = float(rec[i])
+                elif attr_type == "TRANSIT_SEGMENT":
+                    link = network.link(int(rec[1]), int(rec[2]))
+                    for segment in link.segments():
+                        if segment.line.id == rec[0]:
+                            break
+                    else:
+                        raise ExistenceError()
+                    for i, attr in enumerate(header[3:], 3):
+                        segment[attr] = float(rec[i])
 
     def network_calc(self, *args, **kwargs):
         pass
@@ -73,7 +302,12 @@ class MockProject:
     def transit_assignment(self, *args, **kwargs):
         pass
 
-    def congested_assignment(self, *args, **kwargs):
+    def congested_assignment(self, transit_assignment_spec, class_names,
+                             congestion_function, stopping_criteria,
+                             log_worksheets, scenario, save_strategies):
+        self.create_extra_attribute(
+            "TRANSIT_SEGMENT", "@base_timtr", "", 1.0,
+            overwrite=True, scenario=scenario)
         report = {
             "stopping_criterion": "MAX_ITERATIONS",
             "iterations": [{"number": 1}],
@@ -92,7 +326,7 @@ Modeller = namedtuple("Modeller", "emmebank")
 
 class EmmeBank:
     def __init__(self):
-        self._scenarios = {19: Scenario(19)}
+        self._scenarios = {}
         self._matrices = {}
         self._functions = {}
 
@@ -176,7 +410,7 @@ class Scenario:
             if idx in network._extra_attr[attr_type]:
                 return network._extra_attr[attr_type][idx]
 
-    def create_extra_attribute(self, attr_type, idx, default_value=0):
+    def create_extra_attribute(self, attr_type, idx, default_value=0.0):
         network = self.get_network()
         if idx in network._extra_attr[attr_type]:
             raise ExistenceError("Extra attribute already exists: {}".format(
@@ -201,7 +435,7 @@ class ExtraAttribute:
         self.default_value = default_value
         self.scenario = scenario
 
-    def initialize(self, value=0):
+    def initialize(self, value=0.0):
         self.default_value = value
         network = self.scenario.get_network()
         for obj in network._objects[self.type]():
@@ -251,12 +485,15 @@ class Network:
     def modes(self):
         return iter(self._modes.values())
 
-    def create_mode(self, idx):
+    def create_mode(self, mode_type, idx):
+        if not isinstance(idx, str) or len(idx) != 1:
+            raise Exception("Invalid mode ID: " + idx)
         mode = Mode(idx)
         self._modes[idx] = mode
         return mode
 
     def node(self, idx):
+        idx = int(idx)
         if idx in self._nodes:
             return self._nodes[idx]
 
@@ -269,7 +506,8 @@ class Network:
     def regular_nodes(self):
         return iter(self._regular_nodes.values())
 
-    def create_node(self, idx, is_centroid=False):
+    def create_node(self, idx, is_centroid):
+        idx = int(idx)
         node = Node(self, idx, is_centroid)
         self._nodes[idx] = node
         if is_centroid:
@@ -288,7 +526,9 @@ class Network:
 
     def create_link(self, i_node_id, j_node_id, modes):
         modes = [self.mode(str(mode)) for mode in modes]
-        link = Link(self, self.node(i_node_id), self.node(j_node_id), modes)
+        link = Link(
+            self, self._nodes[int(i_node_id)], self._nodes[int(j_node_id)],
+            modes)
         self._links["{}-{}".format(i_node_id, j_node_id)] = link
         return link
 
@@ -315,8 +555,7 @@ class Network:
         return iter(self._segments)
 
     def create_transit_line(self, idx, transit_vehicle_id, itinerary):
-        line = TransitLine(
-                    self, idx, self.transit_vehicle(transit_vehicle_id))
+        line = TransitLine(self, idx, transit_vehicle_id)
         self._lines[idx] = line
         for i in range(len(itinerary) - 1):
             link = self.link(itinerary[i], itinerary[i + 1])
@@ -385,6 +624,8 @@ class Node(NetworkObject):
         NetworkObject.__init__(self, network, network._extra_attr["NODE"])
         self.is_centroid = is_centroid
         self.number = idx
+        self.x = 0.0
+        self.y = 0.0
         self.label = ""
 
     @property
@@ -403,11 +644,6 @@ class Link(NetworkObject):
         self.num_lanes = 1
         self.volume_delay_func = 0
         self.auto_time = 0.1
-        self._extra_attr["@hinta"] = 0.0
-        self._extra_attr["@hinta_aht"] = 0.0
-        self._extra_attr["@hinta_pt"] = 0.0
-        self._extra_attr["@hinta_iht"] = 0.0
-        self._extra_attr["@pyoratieluokka"] = 0.0
         self._segments = []
 
     @property
@@ -432,9 +668,6 @@ class TransitLine(NetworkObject):
         self.id = idx
         self.vehicle = vehicle
         self.headway = 0.01
-        self._extra_attr["@hw_aht"] = 0.01
-        self._extra_attr["@hw_pt"] = 0.01
-        self._extra_attr["@hw_iht"] = 0.01
         self._segments = []
 
     @property
@@ -444,6 +677,14 @@ class TransitLine(NetworkObject):
     @id.setter
     def id(self, idx):
         self._id = idx
+
+    @property
+    def vehicle(self):
+        return self._vehicle
+
+    @vehicle.setter
+    def vehicle(self, vehicle_id):
+        self._vehicle = self.network._vehicles[vehicle_id]
 
     @property
     def mode(self):
@@ -464,7 +705,6 @@ class TransitSegment(NetworkObject):
         self.link = link
         self.transit_time_func = 0
         self.dwell_time = 0.01
-        self._extra_attr["@base_timtr"] = 0.0
 
     @property
     def id(self):
