7335d81c3c52873c51bd1a570904c6e8ec64390b
West Jens <Jens.West@hsl.fi>
2021-06-08 11:43:49 +0300

    Refer to i-node for alighting as well

--- Scripts/assignment/emme_assignment.py
+++ Scripts/assignment/emme_assignment.py
@@ -298,25 +298,24 @@ class EmmeAssignmentModel(AssignmentModel):
             if line.mode.id in param.stop_codes:
                 stop_codes = param.stop_codes[line.mode.id]
                 for segment in line.segments():
-                    alighting = segment.j_node.data2 in stop_codes
-                    boarding = segment.i_node.data2 in stop_codes
+                    is_stop = segment.i_node.data2 in stop_codes
                     if line.mode.id in "de":
                         # Non-HSL bus lines
                         not_hsl = segment.i_node.label not in param.hsl_area
                         if line.id[-1] == '1':
                             # Line starts in HSL area
-                            segment.allow_alightings = not_hsl and alighting
-                            segment.allow_boardings = boarding
+                            segment.allow_alightings = not_hsl and is_stop
+                            segment.allow_boardings = is_stop
                         elif line.id[-1] == '2':
                             # Line ends in HSL area
-                            segment.allow_alightings = alighting
-                            segment.allow_boardings = not_hsl and boarding
+                            segment.allow_alightings = is_stop
+                            segment.allow_boardings = not_hsl and is_stop
                         else:
                             raise ValueError(
                                 "Unknown direction code for line " + line.id)
                     else:
-                        segment.allow_alightings = alighting
-                        segment.allow_boardings = boarding
+                        segment.allow_alightings = is_stop
+                        segment.allow_boardings = is_stop
         self.mod_scenario.publish_network(network)
 
     def _create_attributes(self, scenario, extra):
