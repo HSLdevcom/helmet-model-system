e88721084331b8c87a8766f6044d8728a267be7e
atte-wsp <atte.supponen@wsp.com>
2021-06-16 13:14:34 +0300

    Get AssignmentPeriod dist unit cost from zonedata
    * Currently used in AssignmentPeriod (not EmmeAssignment)

--- Scripts/assignment/emme_assignment.py
+++ Scripts/assignment/emme_assignment.py
@@ -43,7 +43,6 @@ class EmmeAssignmentModel(AssignmentModel):
                 emme_context, save_matrices=save_matrices)
             for i, tp in enumerate(["aht", "pt", "iht"])]
         # default value for dist, modelsystem sets new from zonedata
-        self.dist_unit_cost = param.dist_unit_cost
         self.day_scenario = self.emme_project.modeller.emmebank.scenario(
             first_scenario_id + 1)
 
