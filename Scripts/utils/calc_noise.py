f463ed6229b016c2f338c1c584812b6c6631950a
West Jens <Jens.West@hsl.fi>
2022-02-09 14:31:19 +0200

    Add noise zone width to link print

---
+++ Scripts/utils/calc_noise.py
@@ -0,0 +1,55 @@
+from math import log10
+
+import parameters.assignment as param
+
+class NoiseModel:
+    def __init__(self, morning_network, light_modes, heavy_modes):
+        self.morning_network = morning_network
+        self.light_modes = light_modes
+        self.heavy_modes = heavy_modes
+        self.car_morning = "@car_time_aht"
+
+    def calc_noise(self, link):
+        traffic = sum([link[mode] for mode in self.light_modes])
+        rlink = link.reverse_link
+        if rlink is None:
+            reverse_traffic = 0
+        else:
+            reverse_traffic = sum([rlink[mode] for mode in self.light_modes])
+        cross_traffic = (param.years_average_day_factor
+                            * param.share_7_22_of_day
+                            * (traffic+reverse_traffic))
+        heavy = sum([link[mode] for mode in self.heavy_modes])
+        traffic = max(traffic, 0.01)
+        heavy_share = heavy / (traffic+heavy)
+
+        # Calculate speed
+        link = self.morning_network.link(link.i_node, link.j_node)
+        rlink = link.reverse_link
+        if reverse_traffic > 0:
+            speed = (60 * 2 * link.length
+                        / (link[self.car_morning]+rlink[self.car_morning]))
+        else:
+            speed = (0.3*(60*link.length/link[self.car_morning])
+                        + 0.7*link.data2)
+        speed = max(speed, 50.0)
+
+        # Calculate start noise
+        if speed <= 90:
+            heavy_correction = (10*log10((1-heavy_share)
+                                + 500*heavy_share/speed))
+        else:
+            heavy_correction = (10*log10((1-heavy_share)
+                                + 5.6*heavy_share*(90/speed)**3))
+        start_noise = ((68 + 30*log10(speed/50)
+                        + 10*log10(cross_traffic/15/1000)
+                        + heavy_correction)
+            if cross_traffic > 0 else 0)
+
+        # Calculate noise zone width
+        func = param.noise_zone_width
+        for interval in func:
+            if interval[0] <= start_noise < interval[1]:
+                zone_width = func[interval](start_noise - interval[0])
+                break
+        return zone_width
--- Scripts/utils/print_links.py
+++ Scripts/utils/print_links.py
@@ -1,9 +1,18 @@
+from utils.calc_noise import NoiseModel
+
+
 def print_links(network, resultdata):
     attr_names = network.attributes("LINK")
-    resultdata.print_line("Link\t" + "\t".join(attr_names), "links")
+    resultdata.print_line(
+        "Link\t" + "\t".join(attr_names) + "\tNoise_zone_width", "links")
+    noisemodel = NoiseModel(
+        network, ("@car_work", "@car_leisure", "@van"),
+        ("@truck", "@trailer_truck"))
     for link in network.links():
         wkt = "LINESTRING ({} {}, {} {})".format(
             link.i_node.x, link.i_node.y, link.j_node.x, link.j_node.y)
         attrs = "\t".join([str(link[attr]) for attr in attr_names])
-        resultdata.print_line(wkt + "\t" + attrs, "links")
+        noise_zone_width = noisemodel.calc_noise(link)
+        resultdata.print_line(
+            wkt + "\t" + attrs + "\t" + noise_zone_width, "links")
     resultdata.flush()
