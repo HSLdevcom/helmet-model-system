e209a328ac8b8aa84c224cc3f35f0ed4baf7b311
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-09-15 18:58:34 +0300

    Fix typo

--- Scripts/assignment/assignment_period.py
+++ Scripts/assignment/assignment_period.py
@@ -478,7 +478,7 @@ class AssignmentPeriod(Period):
         dist = self._get_matrix("dist", ass_class)
         time = gcost - vot_inv*self.dist_unit_cost*dist
         if ass_class not in ("trailer_truck", "truck"):
-            time =- vot_inv*cost
+            time -= vot_inv*cost
         self._set_matrix(ass_class, time, "time")
         return time
 
