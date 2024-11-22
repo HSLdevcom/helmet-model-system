a04c76166c42eecda71f13f558d14e2157200c29
Sami Mäkinen <sami.makinen@flou.io>
2024-11-22 17:14:37 +0200

    Fix crash when no parking facilities are defined

--- Scripts/transform/park_and_ride_transformer.py
+++ Scripts/transform/park_and_ride_transformer.py
@@ -134,6 +134,21 @@ class ParkAndRideTransformer(ImpedanceTransformerBase):
         cols = (purpose.bounds if purpose.name == "hoo"
             else slice(0, purpose.zone_data.nr_zones))
 
+        if len(self._facilities) == 0: #if there are no P+R facilities (mostly for testing)
+            rows = sum([1 for _ in purpose.zone_numbers])
+            cols = purpose.zone_data.nr_zones
+            inf_mat = np.full((rows,cols), 999.)
+            return {
+                
+                'park_and_ride': {
+                    'cost': inf_mat,
+                    'time': inf_mat,
+                    'dist': inf_mat,
+                    'gen_cost': inf_mat,
+                    'used_facility': np.full((rows, cols), None)
+                }
+            }
+        
         day_imp = {}
         for mode in [car_mode, transit_mode]:
             day_imp[mode] = defaultdict(float)
