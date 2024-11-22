b28e563bc76c7d275ab9c8ade4990c2b161bd2fe
Sami Mäkinen <sami.makinen@flou.io>
2024-11-22 15:34:55 +0200

    Implement functionality to add congestion penalty to p&r facilities

--- Scripts/transform/park_and_ride_transformer.py
+++ Scripts/transform/park_and_ride_transformer.py
@@ -16,6 +16,8 @@ class ParkAndRideFacility(NamedTuple):
     capacity: float
     cost: float
     time: float
+    used_capacity: float
+    adjustments: int
 
 class ParkAndRideTransformer(ImpedanceTransformerBase):
     _zone_data: ZoneData
@@ -30,9 +32,69 @@ class ParkAndRideTransformer(ImpedanceTransformerBase):
                                                 num,
                                                 zone_data['pnr_capacity'].loc[num],
                                                 zone_data['pnr_cost'].loc[num],
-                                                0.0)
+                                                0.0,
+                                                0.0,
+                                                0)
                             for i, num in pnr_centroids]
+        self.used_facility: np.ndarray = None
+
+    def get_pnr_map(self) -> Dict[int,int]:
+        """
+        Get mapping pnr_zone_emme_number -> pnr_zone_order_index
+        """
+        return {f.zone_id: f.zone_offset for f in self._facilities}
     
+    
+    def apply_crowding_penalty(self) -> int:
+        """
+        Adjusts the time penalty for facilities based on their capacity usage.
+
+        This method iterates over the facilities and applies a time penalty if the 
+        facility is over capacity, or reduces the time penalty if the facility is 
+        significantly under capacity.
+
+        Returns:
+            int: The number of facilities that had their time penalty modified.
+        """
+        modified = 0
+        BASE_ADJUSTMENT = 3.0 # Base adjustment will be divided by the number of adjustments
+        for f in self._facilities:
+            if f.used_capacity > f.capacity:
+                if f.adjustments%2 == 0:
+                    f.adjustments += 1 # Use odd adjustments values to increase the time penalty
+                # Increase the time penaltyif the facility is over capacity
+                f.time += BASE_ADJUSTMENT/f.adjustments 
+                modified += 1
+                continue
+            if f.used_capacity < 0.75*f.capacity and f.time > 0:
+                if f.adjustments%2 != 0:
+                    f.adjustments += 1 # Use even adjustments values to decrease the time penalty
+                # reduce the time penalty if the facility is significantly under capacity
+                f.time = max(0, f.time-BASE_ADJUSTMENT/f.adjustments)
+                modified += 1
+        return modified
+    
+    def assign_demand(self, demand: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Assigns demand to car and transit facilities.
+        Parameters:
+        demand (np.ndarray): A 2D array representing the demand matrix.
+        Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple containing two 2D arrays:
+            - car_demand: Car demand into facilities.
+            - transit_demand: Transit demand from the facilities.
+        """
+        car_demand = np.zeros_like(demand)
+        transit_demand = np.zeros_like(demand)
+        
+        for i, facility in enumerate(self._facilities):
+            d = demand[self.used_facility == i]
+            car_demand[:,facility.zone_offset] += d.sum(axis=1)
+            transit_demand[facility.zone_offset,:] += d.sum(axis=0)
+            facility.used_capacity = d.sum()
+        
+        return car_demand, transit_demand
+            
     def transform(self,
                   purpose: Purpose,
                   impedance: Dict[str, Dict[str, Dict[str, np.ndarray]]]
@@ -98,12 +160,13 @@ class ParkAndRideTransformer(ImpedanceTransformerBase):
         
         min_index = np.argmin(generalized_cost, axis=0)[None, :, :]#, keepdims=True)
         f_zones = np.array([f.zone_id for f in self._facilities])[:,None,None]
+        self.used_facility = np.take_along_axis(f_zones, min_index, axis=0)[0]
         day_impedance = {
             'park_and_ride': {
                 'cost': (np.take_along_axis(cost, min_index, axis=0)[0]),
                 'time': np.take_along_axis(time, min_index, axis=0)[0],
                 'gen_cost': np.take_along_axis(generalized_cost, min_index, axis=0)[0],
-                'used_facility': np.take_along_axis(f_zones, min_index, axis=0)[0]
+                'used_facility': self.used_facility
             }
         }
         return day_impedance
