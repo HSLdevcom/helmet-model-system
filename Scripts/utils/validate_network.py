21a8ae7162f9ce4bf88bff4033381040ca54c338
Santeri Hiitola <santeri.hiitola@outlook.com>
2025-01-10 13:22:30 +0200

    Fix accessible centroids validation

--- Scripts/utils/validate_network.py
+++ Scripts/utils/validate_network.py
@@ -69,7 +69,8 @@ def validate(network, fares=None):
     required_modes = {'a', 'f', 'c'}  # Modes for pedestrians, cyclists, and cars
 
     for centroid in network.centroids():
-        accessible_modes = {mode for link in centroid.incoming_links() for mode in link.modes}
+        accessible_modes = {mode.id for link in centroid.incoming_links() for mode in link.modes}
+        print(accessible_modes)
 
         if not required_modes.issubset(accessible_modes):
             unaccessible_centroids.append(centroid.id)
