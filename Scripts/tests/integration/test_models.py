ff47ff50c7d975eeafb2cd73a7005ebe0d9fb233
hsl-petrhaj <petr.hajduk@hsl.fi>
2025-02-06 15:34:30 +0200

    test fix after new car usage model

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -56,7 +56,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.2880867809652605)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.3038856437557159)
         
         print("Model system test done")
     
