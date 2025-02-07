3133f45e37f42843fda82533e25e8337a7076cf2
Petr Hajduk <petr.hajduk@hsl.fi>
2025-02-07 16:16:13 +0200

    Updated car driver share and sec dest coefficients

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -56,7 +56,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.3038856437557159)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.3068142270430023)
         
         print("Model system test done")
     
