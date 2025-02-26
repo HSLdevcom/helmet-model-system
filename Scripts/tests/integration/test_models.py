c0f274415ef10528c3737875a4bfb4994534a4f5
Petr Hajduk <petr.hajduk@hsl.fi>
2025-02-26 17:33:22 +0200

    Calibrated mode choice by mode and origin

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -61,7 +61,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.3068142270430023)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.2639159380383261)
         
         print("Model system test done")
     
