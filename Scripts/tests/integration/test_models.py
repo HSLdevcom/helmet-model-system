9fedac6bfa6ea838f5e8be8b9c32506c14f5c72d
Petr Hajduk <petr.hajduk@hsl.fi>
2025-08-20 12:04:44 +0300

    tests adjusted

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -61,7 +61,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.2202346412061351)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.21700013864246281)
         
         print("Model system test done")
     
