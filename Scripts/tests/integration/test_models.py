45442c0c0ae6b36c429a6733141be12f3e1a60b3
Petr Hajduk <petr.hajduk@hsl.fi>
2025-08-26 12:22:09 +0300

    Tesing with updated impedance shares

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -61,7 +61,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.22689211803987006)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.2269648011080445)
         
         print("Model system test done")
     
