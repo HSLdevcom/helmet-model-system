02ad1408ee5919b94734cf8da990e1248c853f6d
Petr Hajduk <petr.hajduk@hsl.fi>
2026-03-11 17:33:11 +0200

    Fix tests for updated zone data

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -61,7 +61,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.21777550284903216)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.22094077163855128)
         
         print("Model system test done")
     
