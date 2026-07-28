b4c9e95aa2eb947d48fcde92b119def27899b94a
Petr Hajduk <petr.hajduk@hsl.fi>
2026-07-28 17:13:27 +0300

    Updating tests because travel modes now includes park and ride

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -61,7 +61,7 @@ class ModelTest(unittest.TestCase):
         self._validate_impedances(impedance["iht"])
 
         # Check that model result does not change
-        self.assertAlmostEquals(model.mode_share[0]["car"], 0.21777550284903216)
+        self.assertAlmostEquals(model.mode_share[0]["car"], 0.21754170720172142)
         
         print("Model system test done")
     
