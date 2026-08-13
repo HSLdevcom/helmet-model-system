c944ba4c8b9a50502852b86d0deb35f3703b73e8
Santeri <santeri.hiitola@outlook.com>
2026-08-13 12:18:58 +0300

    Added validation, fixed possible KeyErrors, improved logic

--- Scripts/utils/modify_network.py
+++ Scripts/utils/modify_network.py
@@ -47,7 +47,10 @@ def add_bus_stops(network):
                              "194397-194395", "212415-204085", "204085-213798","93047-93048"])
     whitelist_line_ids = set(["1094A1"])
     for line in network.transit_lines():
-        if line.mode.id in param.stop_codes and line["@custom_line"] != 1:
+        if '@custom_line' in line and line['@custom_line'] == 1:
+            log.debug(f"Bus stops not added to {line.id} because it is marked as a custom line.")
+            continue
+        if line.mode.id in param.stop_codes:
             stop_codes = param.stop_codes[line.mode.id]
             stop_distance = 0
             max_stop_distance = 0
